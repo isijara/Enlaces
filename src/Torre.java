e25735d0f5cfc88170dba138aa0aa7f83d400562
Ramon Isijara <risijara@gmail.com>
2013-09-02 16:52:02 -0600

    Primer commit, agregada clase Torre y ArchivoEntrada

---
+++ src/Torre.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Torre {
+
+	private String nombre;
+	private Vector <Torre>  vector_torres;// = new Vector<Torre> ();
+	private Vector <String> vector_ruta ;// = new Vector<String>();
+	private boolean estatus_busqueda;
+	
+	public Torre ( String nombre ) {
+		this.nombre = nombre;
+		this.vector_torres = new Vector<Torre> ();
+		this.vector_ruta   = new Vector<String>();
+		this.estatus_busqueda = false;
+	}
+	
+	private boolean buscar_enlace(){
+		
+		return true;
+	}
+	
+	public Vector <Torre> get_vector_torres() {
+		return this.vector_torres;
+	}
+	
+	public Vector <String> get_vector_ruta() {
+		return this.vector_ruta;
+	}
+	
+	public String get_nombre() {
+		return this.nombre;
+	}
+	
+	public void set_estatus_busqueda(boolean estatus) {
+		this.estatus_busqueda = estatus;
+	}
+	
+	public boolean get_estatus_busqueda() {
+		return this.estatus_busqueda;
+	}
+	
+}
